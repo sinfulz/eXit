f66cf43cbe71342d0a4f10101f4eec8d8ad5d427
lucablackwell <luca@lucablackwell.com>
2021-12-02 14:07:59 +0000

    Slow print

--- eXit.php
+++ eXit.php
@@ -7,6 +7,8 @@
 # look
 # get on boat
 
+
+# Colours
 function cyan($text) {
     return "\e[0;36m$text\e[0m";
 }
@@ -19,109 +21,150 @@ function white_bold($text) {
     return "\e[1;37m$text\e[0m";
 }
 
-function slow_print($line) {
+function black($text) {
+    return "\e[1;30m$text\e[0m";
+}
+
+function slow_print($time, $line, $newline) {
     $line_arr = preg_split("//u", $line, -1, PREG_SPLIT_NO_EMPTY);
     foreach ($line_arr as $char) {
         echo $char;
-        usleep(1100);
+        usleep($time);
     }
-    echo "\n";
+    if ($newline) {
+        echo "\n";
+    }
 }
 
+# Time variables
+$time_logo = 1100;
+$time_text = 28000;
 
-function display_logo() {
-    slow_print('████████████████████████████████████████████████████████████████████████████████')."\n";
-    slow_print('███████████████████████████████'.cyan('▓╫╫╫╫').'█████'.cyan('╫╫╫╫▓').'██████████████████████████████████')."\n";
-    slow_print('███████████████████'.cyan('╬╬╬╬╬╬╬╬').'█████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╣').'█████████████'.cyan('╬╬╬╬').'██████████████████')."\n";
-    slow_print('█████████████████'.cyan('╬╬╬▓').'███'.cyan('▓╬╬╬╣').'████'.cyan('▓╬╬╬╬╬╬╬╬╬').'███████'.cyan('▓╬╬╬╬').'██'.cyan('▓▓╬╬╬╬╣▓▓').'██████████████')."\n";
-    slow_print('██████████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣').'████'.cyan('▓╬╬╬╬╬╬╬╬╬').'████████'.cyan('╬╬╬╬╬').'█████'.cyan('╬╬╬╬').'███████████████')."\n";
-    slow_print('█████████████'.cyan('╬╬╬╬▓').'███████████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬').'███████'.cyan('▓╬╬╬╬').'█████'.cyan('▓╬╬╬╬╬').'█████████████')."\n";
-    slow_print('███████████'.cyan('▓╬╬╬╬╬').'███'.cyan('╬╬╬╬╬╣').'█████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬').'██████'.cyan('╬╬╬╬╬╬').'████'.cyan('▓╬╬╬╬╣').'████████████')."\n";
-    slow_print('██████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓').'████'.cyan('╬╬╬╬╬╬╬▓').'██'.cyan('╬╬╬╬╬╬╬╬').'█████'.cyan('╬╬╬╬╬╬').'██████'.cyan('▓╬╬╬╬╬').'██████████')."\n";
-    slow_print('█████████████'.cyan('╫╫╫╫╫╫╫').'██████'.cyan('╬╫╫╫╫╫╫╫╫').'█████'.cyan('▓╫╫╫╫╫╫╫╢').'█████'.cyan('╫╫╫╫╫').'████████'.cyan('╫╫╫╫╫╫╫╢').'█████')."\n";
-    slow_print('██████████████████████████'.cyan('╬╬╬╬╬╬╬▓').'███████'.cyan('▓╬╬╬╬╬╬╬').'██████████████████████'.cyan('▓▓▓').'██████')."\n";
-    slow_print('███████████████████████'.cyan('▓╬╬╬╬╬╬╬▓').'██████████'.cyan('╬╬╬╬╬╬╬╬╬╬').'████████████████████████████')."\n";
-    slow_print('████████████████████████████████████████████████████████████████████████████████')."\n";
+function startup($time_logo) {
+    slow_print($time_logo, '████████████████████████████████████████████████████████████████████████████████', true);
+    slow_print($time_logo, '███████████████████████████████'.cyan('▓╫╫╫╫').'█████'.cyan('╫╫╫╫▓').'██████████████████████████████████', true);
+    slow_print($time_logo, '███████████████████'.cyan('╬╬╬╬╬╬╬╬').'█████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╣').'█████████████'.cyan('╬╬╬╬').'██████████████████', true);
+    slow_print($time_logo, '█████████████████'.cyan('╬╬╬▓').'███'.cyan('▓╬╬╬╣').'████'.cyan('▓╬╬╬╬╬╬╬╬╬').'███████'.cyan('▓╬╬╬╬').'██'.cyan('▓▓╬╬╬╬╣▓▓').'██████████████', true);
+    slow_print($time_logo, '██████████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣').'████'.cyan('▓╬╬╬╬╬╬╬╬╬').'████████'.cyan('╬╬╬╬╬').'█████'.cyan('╬╬╬╬').'███████████████', true);
+    slow_print($time_logo, '█████████████'.cyan('╬╬╬╬▓').'███████████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬').'███████'.cyan('▓╬╬╬╬').'█████'.cyan('▓╬╬╬╬╬').'█████████████', true);
+    slow_print($time_logo, '███████████'.cyan('▓╬╬╬╬╬').'███'.cyan('╬╬╬╬╬╣').'█████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬').'██████'.cyan('╬╬╬╬╬╬').'████'.cyan('▓╬╬╬╬╣').'████████████', true);
+    slow_print($time_logo, '██████████'.cyan('╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓').'████'.cyan('╬╬╬╬╬╬╬▓').'██'.cyan('╬╬╬╬╬╬╬╬').'█████'.cyan('╬╬╬╬╬╬').'██████'.cyan('▓╬╬╬╬╬').'██████████', true);
+    slow_print($time_logo, '█████████████'.cyan('╫╫╫╫╫╫╫').'██████'.cyan('╬╫╫╫╫╫╫╫╫').'█████'.cyan('▓╫╫╫╫╫╫╫╢').'█████'.cyan('╫╫╫╫╫').'████████'.cyan('╫╫╫╫╫╫╫╢').'█████', true);
+    slow_print($time_logo, '██████████████████████████'.cyan('╬╬╬╬╬╬╬▓').'███████'.cyan('▓╬╬╬╬╬╬╬').'██████████████████████'.cyan('▓▓▓').'██████', true);
+    slow_print($time_logo, '███████████████████████'.cyan('▓╬╬╬╬╬╬╬▓').'██████████'.cyan('╬╬╬╬╬╬╬╬╬╬').'████████████████████████████', true);
+    slow_print($time_logo, '████████████████████████████████████████████████████████████████████████████████', true);
+}
+
+function shutdown($time_logo) {
+    slow_print($time_logo, black('████████████████████████████████████████████████████████████████████████████████'), true);
+    slow_print($time_logo, black('███████████████████████████████').'▓╫╫╫╫'.black('█████').'╫╫╫╫▓'.black('██████████████████████████████████'), true);
+    slow_print($time_logo, black('███████████████████').'╬╬╬╬╬╬╬╬'.black('█████').'╬╬╬╬╬╬╬╬╬╬╬╬╣'.black('█████████████').'╬╬╬╬'.black('██████████████████'), true);
+    slow_print($time_logo, black('█████████████████').'╬╬╬▓'.black('███').'▓╬╬╬╣'.black('████').'▓╬╬╬╬╬╬╬╬╬'.black('███████').'▓╬╬╬╬'.black('██').'▓▓╬╬╬╬╣▓▓'.black('██████████████'), true);
+    slow_print($time_logo, black('██████████████').'╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣'.black('████').'▓╬╬╬╬╬╬╬╬╬'.black('████████').'╬╬╬╬╬'.black('█████').'╬╬╬╬'.black('███████████████'), true);
+    slow_print($time_logo, black('█████████████').'╬╬╬╬▓'.black('███████████████').'╬╬╬╬╬╬╬╬╬╬╬'.black('███████').'▓╬╬╬╬'.black('█████').'▓╬╬╬╬╬'.black('█████████████'), true);
+    slow_print($time_logo, black('███████████').'▓╬╬╬╬╬'.black('███').'╬╬╬╬╬╣'.black('█████').'╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬'.black('██████').'╬╬╬╬╬╬'.black('████').'▓╬╬╬╬╣'.black('████████████'), true);
+    slow_print($time_logo, black('██████████').'╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓'.black('████').'╬╬╬╬╬╬╬▓'.black('██').'╬╬╬╬╬╬╬╬'.black('█████').'╬╬╬╬╬╬'.black('██████').'▓╬╬╬╬╬'.black('██████████'), true);
+    slow_print($time_logo, black('█████████████').'╫╫╫╫╫╫╫'.black('██████').'╬╫╫╫╫╫╫╫╫'.black('█████').'▓╫╫╫╫╫╫╫╢'.black('█████').'╫╫╫╫╫'.black('████████').'╫╫╫╫╫╫╫╢'.black('█████'), true);
+    slow_print($time_logo, black('██████████████████████████').'╬╬╬╬╬╬╬▓'.black('███████').'▓╬╬╬╬╬╬╬'.black('██████████████████████').'▓▓▓'.black('██████'), true);
+    slow_print($time_logo, black('███████████████████████').'▓╬╬╬╬╬╬╬▓'.black('██████████').'╬╬╬╬╬╬╬╬╬╬'.black('████████████████████████████'), true);
+    slow_print($time_logo, black('████████████████████████████████████████████████████████████████████████████████'), true);
+}
+
+function shutdown_space($time_logo) {
+    slow_print($time_logo, '                                                                                ', true);
+    slow_print($time_logo, '                               ▓╫╫╫╫     ╫╫╫╫▓                                  ', true);
+    slow_print($time_logo, '                   ╬╬╬╬╬╬╬╬     ╬╬╬╬╬╬╬╬╬╬╬╬╣             ╬╬╬╬                  ', true);
+    slow_print($time_logo, '                 ╬╬╬▓   ▓╬╬╬╣    ▓╬╬╬╬╬╬╬╬╬       ▓╬╬╬╬  ▓▓╬╬╬╬╣▓▓              ', true);
+    slow_print($time_logo, '              ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╣    ▓╬╬╬╬╬╬╬╬╬        ╬╬╬╬╬     ╬╬╬╬               ', true);
+    slow_print($time_logo, '             ╬╬╬╬▓               ╬╬╬╬╬╬╬╬╬╬╬       ▓╬╬╬╬     ▓╬╬╬╬╬             ', true);
+    slow_print($time_logo, '           ▓╬╬╬╬╬   ╬╬╬╬╬╣     ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬      ╬╬╬╬╬╬    ▓╬╬╬╬╣            ', true);
+    slow_print($time_logo, '          ╬╬╬╬╬╬╬╬╬╬╬╬╬▓▓    ╬╬╬╬╬╬╬▓  ╬╬╬╬╬╬╬╬     ╬╬╬╬╬╬      ▓╬╬╬╬╬          ', true);
+    slow_print($time_logo, '             ╫╫╫╫╫╫╫      ╬╫╫╫╫╫╫╫╫     ▓╫╫╫╫╫╫╫╢     ╫╫╫╫╫        ╫╫╫╫╫╫╫╢     ', true);
+    slow_print($time_logo, '                          ╬╬╬╬╬╬╬▓       ▓╬╬╬╬╬╬╬                      ▓▓▓      ', true);
+    slow_print($time_logo, '                       ▓╬╬╬╬╬╬╬▓          ╬╬╬╬╬╬╬╬╬╬                            ', true);
+    slow_print($time_logo, '                                                                                ', true);
 }
 
 
 
 
 # start
-display_logo();
-readline('Press the enter button to continue...');
+#shutdown($time_logo);
+startup($time_logo);
+readline(slow_print($time_text, 'Press the enter button to continue...', false));
 
 
 
 
-function trapped() {
-    echo 'You\'re trapped in a dungeon with your friend.'."\n".'You see a barrel. What do you do?'."\n";
+function trapped($time_text) {
+    slow_print($time_text, 'You\'re trapped in a dungeon with your friend.'."\n".'You see a barrel. What do you do?', true);
 }
-trapped();
+trapped($time_text);
 
 # tunnel
-function dungeon() {
+function dungeon($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'move barrel') {
-        echo 'The barrel rolls aside and you find a secret tunnel.'."\n".'What do you do?'."\n";
+        slow_print($time_text, 'The barrel rolls aside and you find a secret tunnel.'."\n".'What do you do?', true);
     } else {
-        echo 'You can\'t do that here.'."\n";
+        slow_print($time_text, 'You can\'t do that here.', true);
     }
 }
-dungeon();
+dungeon($time_text);
 
 # note
-function bye() {
+function bye($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'enter tunnel') {
-        echo 'You start to escape but your friend is too weak to go with you. They hand you a note.'."\n".'What do you do?'."\n";
+        slow_print($time_text, 'You start to escape but your friend is too weak to go with you. They hand you a note.'."\n".'What do you do?', true);
     } else {
-        echo 'You can\'t type that.'."\n";
+        slow_print($time_text, 'You can\'t type that.', true);
     }
 }
-bye();
+bye($time_text);
 
 # leave
-function note() {
+function note($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'read note') {
-        echo 'It is too dark to read the note.'."\n".'What do you do?'."\n";
+        slow_print($time_text, 'It is too dark to read the note.'."\n".'What do you do?', true);
     } else {
-        echo 'You can\'t do that here.'."\n";
+        slow_print($time_text, 'You can\'t do that here.', true);
     }
 }
-note();
+note($time_text);
 
 # beach
-function leave() {
+function leave($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'leave') {
-        echo 'You crawl through the tunnel and the tunnel leads to a beach.'."\n".'What do you do?'."\n";
+        slow_print($time_text, 'You crawl through the tunnel and the tunnel leads to a beach.'."\n".'What do you do?', true);
     } else {
-        echo 'You can\'t do that here.'."\n";
+        slow_print($time_text, 'You can\'t do that here.', true);
     }
 }
-leave();
+leave($time_text);
 
 # boat
-function look() {
+function look($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'look') {
-        echo 'In the water you see a boat.'."\n".'What do you do?'."\n";
+        slow_print($time_text, 'In the water you see a boat.'."\n".'What do you do?', true);
     } else {
-        echo 'You can\'t do that here.'."\n";
+        slow_print($time_text, 'You can\'t do that here.', true);
     }
 }
-look();
+look($time_text);
 
 # eXit
-function get() {
+function get($time_text) {
     echo cyan_bold('> ');
     if (readline('') == 'get on boat') {
-        echo 'Congratulations, you\'re heading to a new world!'."\n";
+        slow_print($time_text, 'Congratulations, you\'re heading to a new world!', true);
     }
 }
-get();
+get($time_text);
+shutdown_space($time_logo);
 
 
 ?>
