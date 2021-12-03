d7eb702c93c75dab539bb5d2c12447d5ac77a3f1
lucablackwell <lucablackwell2005@gmail.com>
2021-12-03 14:34:06 +0000

    Full optional eXit demo ting

--- eXit.php
+++ eXit.php
@@ -38,7 +38,8 @@ function slow_print($time, $line, $newline) {
 
 # Time variables
 $time_logo = 1100;
-$time_text = 28000;
+$time_text = 26000;
+#$time_text = 1000; # debug speed
 
 function startup($time_logo) {
     slow_print($time_logo, '████████████████████████████████████████████████████████████████████████████████', true);
@@ -86,13 +87,120 @@ function shutdown_space($time_logo) {
 }
 
 
+function congrats($time_text) {
+    echo "\n";
+    slow_print($time_text, 'Congratulations. You have passed the test.', true);
+    $key = 'xxxet-xi--ee--tteitxxeeetiteitxe-ixxii--ee--ieeetetxitextxexx-ii-ti-xi--t-i-e--t-iiit--etxeiex-xixit-xt-iett-xxex-xtetieix--ti-e-xxxxeixt--xxeieixeitee-eiee-xt-x-i-te-txxititxeexxte-titixi-tei-xieexx-xitittx-etitx--ie--ii-exit-iixt-ei---xxitx-itiexeitxeetxtetxiitxi-e--i-x-ixtexex---e-ttttxiietitiieeixixtiexx--exexetxxeei--xi-ixitte-tei-ttiexee-xtiitixxe-tti-e-xiiieix-ex-teeixex-xieix-eiie-exx--etex-etiittett--teii-tie-extixetiexi-xit-e-tei-i-tte-tetxtiix-ieix-ttexxieiiiet--xtietieiietxx-xexeit-xt-ttexii-tei-itxi-t-t-ixxix-ei-i-txixitetei-i-xeeexixte-xxitxittti-tte--te-xetiittxxiexeteixxx';
+    slow_print($time_text, 'Here is your key:', true);
+    echo "\n";
+    slow_print(2500, str_shuffle($key), true);
+    echo "\n";
+    slow_print($time_text, 'Use it wisely.', true);
+    echo "\n";
+    exit;
+}
 
+function show_inventory($time_text, $inventory) {
+    echo "\n";
+    slow_print($time_text, "        INVENTORY", true);
+    foreach ($inventory as $item => $has) {
+        if ($has == 1) {
+            slow_print($time_text, "| $item", true);
+        }
+    }
+}
 
 # start
-#shutdown($time_logo);
-startup($time_logo);
-readline(slow_print($time_text, 'Press the enter button to continue...', false));
+shutdown_space($time_logo);
+#startup($time_logo);
+slow_print($time_text, 'Press the enter button to continue..', false);
+if (readline('.') == 'eXit') {
+    $inventory = [
+        'Ruler' => 'false',
+        'Key' => 'false'
+    ];
+    # desk
+    slow_print($time_text, "\nYou wake in a cold room. The walls are bare and the carpet is worn.\nThe only light is provided by a dim bulb, which hangs loosely from the ceiling.\nEven the air feels unwelcoming and hostile. The bed you lie on is uncomfortable.\nA rickety wooden desk leans against the wall.", true);
+    $wake_input = readline('> ');
+    $wake_switch = false;
+    while (!$wake_switch) {
+        if (in_array($wake_input, ['look at desk', 'look desk', 'inspect desk', 'go to desk', 'go desk'])) {
+            $wake_switch = true;
+        } elseif (in_array($wake_input, ['look at bed', 'inspect bed', 'look bed'])) {
+            slow_print($time_text, "\nThe sheets are dusty and coarse. It is obvious that no part of the bed has been cleaned for months.", true);
+            $wake_input = readline('> ');
 
+        } else {
+            slow_print($time_text, 'I\'m sorry, I don\'t know what you mean.', true);
+            $wake_input = readline('> ');
+        }
+    }
+    slow_print($time_text, "\nYou approach the desk. The paint is off-white and flaky.\nIt seems to be clinging to the wood for dear life.\nA single sheet of paper and a wooden ruler lay on the desk.\nThe drawer of the desk is slightly open.", true);
+    $desk_input = readline('> ');
+    $desk_switch = false;
+    $drawer_tried_switch = false;
+    $drawer_open_switch = false;
+    while (!$desk_switch) {
+        if (in_array($desk_input, ['look at drawer', 'inspect drawer', 'look drawer', 'open drawer'])) {
+            if ($drawer_open_switch == false) {
+                if ($drawer_tried_switch == false && $inventory['Ruler'] != 1) {
+                    slow_print($time_text, "\nThe drawer lies ajar, but something seems to be jamming it from opening fully.\nYou attempt to clear the blockage, but it is just out of reach.", true);
+                    $drawer_tried_switch = true;
+                } elseif ($drawer_tried_switch == true && $inventory['Ruler'] != 1) {
+                    slow_print($time_text, "\nThe drawer still won't budge. Perhaps there's some sort of instrument that could clear the blockage?", true);
+                } elseif ($drawer_tried_switch == false && $inventory['Ruler'] == 1) {
+                    slow_print($time_text, "\nThe drawer lies ajar, but something seems to be jamming it from opening fully.\nAfter a few tries, you use the ruler to pry the obstruction free.\nThe drawer is empty, but you discover that the blockage was caused by a small black box.\nIt has a lock on the side that requires 5 letters.\n(Say 'lock [combination]' to unlock.)", true);
+                    $drawer_tried_switch = true;
+                    $drawer_open_switch = true;
+                } elseif ($drawer_tried_switch == true && $inventory['Ruler'] == 1) {
+                    slow_print($time_text, "\nAfter a few tries, you use the ruler to pry the obstruction free.\nThe drawer is empty, but you discover that the blockage was caused by a small black box.\nIt has a lock on the side that requires 5 letters.\n(Say 'lock [combination]' to unlock.)", true);
+                    $drawer_open_switch = true;
+                }
+                $desk_input = readline('> ');
+            } else {
+                slow_print($time_text, "\nThe lock in the drawer requires 5 letters.\n(Say 'lock [combination]' to unlock.)", true);
+                $desk_input = readline('> ');
+            }
+        } elseif (substr_count($desk_input, 'lock') == 1) {
+            $combo = substr($desk_input, 5, 5);
+            if (strtolower($combo) == 'xiula') {
+                slow_print($time_text, "\nThe lock clicks open. Peering inside the box, you see a small brass key.\nYou use it to open the door to your room, and eXit.", true);
+                $desk_switch = true;
+                congrats($time_text);
+            } else {
+                slow_print($time_text, "\nIncorrect combination.\n(Say 'lock [combination]' to unlock.)", true);
+                $desk_input = readline('> ');
+            }
+
+
+        } elseif (in_array($desk_input, ['look paper', 'look at paper', 'inspect paper', 'look ruler', 'look at ruler', 'inspect ruler'])) {
+            if ($inventory['Ruler'] == 1) {
+                slow_print($time_text, "\nThe paper reads:\neXit whIle yoU stiLl cAn", true);
+            } else {
+                slow_print($time_text, "\nThe ruler and the paper on the desk are less dusty than the rest of the room.\nYou pick up the ruler.", true);
+                $inventory['Ruler'] = true;
+                show_inventory($time_text, $inventory);
+                slow_print($time_text, "\nYou look at the paper more closely. It reads:\neXit whIle yoU stiLl cAn", true);
+            }
+            $desk_input = readline('> ');
+        } elseif (in_array($desk_input, ['look', 'look desk', 'inspect desk', 'look at desk'])) {
+            if ($drawer_open_switch) {
+                slow_print($time_text, "\nThe paint of the desk is off-white and flaky.\nIt seems to be clinging to the wood for dear life.\nA single sheet of paper lies on the desk.\nThe drawer is open; a locked box sits inside.", true);
+            } else {
+                if ($inventory['Ruler'] == 1) {
+                    slow_print($time_text, "\nThe paint of the desk is off-white and flaky.\nIt seems to be clinging to the wood for dear life.\nA single sheet of paper lies on the desk.\nOne of the drawers is slightly open.", true);
+                } else {
+                    slow_print($time_text, "\nThe paint of the desk is off-white and flaky.\nIt seems to be clinging to the wood for dear life.\nA single sheet of paper and a wooden ruler lay on the desk.\nOne of the drawers is slightly open.", true);
+                }
+            }
+            $desk_input = readline('> ');
+        } else {
+            slow_print($time_text, 'I\'m sorry, I don\'t know what you mean.', true);
+            $desk_input = readline('> ');
+        }
+    }
+}
+echo "\n";
 
 
 
